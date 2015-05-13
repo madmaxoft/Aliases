592be515a04047a1a9af3228f009a98f74b44f7f
Mattes D <github@xoft.cz>
2015-05-13 19:17:26 +0200

    Enhanced the Info.lua documentation.

--- Info.lua
+++ Info.lua
@@ -10,8 +10,19 @@
 g_PluginInfo =
 {
 	Name = "Aliases",
-	Date = "2015-05-10",
-	Description = "Allows players to define aliases for in-game commands and even chat messages. Server admins can define their aliases for console commands.",
+	Date = "2015-05-13",
+	Description =
+	[[
+		This is a plugin for {%a http://mc-server.org}MCServer{%/a} that allows players to define aliases for
+		in-game commands and even chat messages. Server admins can define their aliases for console commands.
+		
+		This makes it easier to use long commands - you can define a shorter version of the command, and even include the initial parameters. For example:
+		{%list}
+		{%li}"/td" -> "/time set day": By using the simple "/td" command you can set the time to day in the world{%/li}
+		{%li}"/ts" -> "/time set": Now you can use commands such as "/ts night" to set night in the world{%/li}
+		{%li}(console) "kx" -> "kick xoft I hate you because": Now you can kick me really fast using the console command "kx no reason"{%/li}
+		{%/list}
+	]],
 	
 	Commands =
 	{
@@ -96,7 +107,26 @@ g_PluginInfo =
 				},  -- set
 			},  -- Subcommands
 		},  -- alias
-	}
+	},  -- ConsoleCommands
+	
+	Permissions =
+	{
+		["aliases.alias.list"] =
+		{
+			Description = "Allows players to show a list of all their currently defined aliases",
+			RecommendedGroups = "everyone",
+		},
+		["aliases.alias.remove"] =
+		{
+			Description = "Allows players to remove an alias they have defined earlier",
+			RecommendedGroups = "everyone",
+		},
+		["aliases.alias.set"] =
+		{
+			Description = "Allows players to define their aliases",
+			RecommendedGroups = "everyone",
+		},
+	},  -- Permissions
 }
 
 
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Allows players to define aliases for in-game commands and even chat messages. Server admins can define their aliases for console commands.
+This is a plugin for [MCServer](http://mc-server.org) that allows players to define aliases for in-game commands and even chat messages. Server admins can define their aliases for console commands.
+
+This makes it easier to use long commands - you can define a shorter version of the command, and even include the initial parameters. For example: 
+ 
+ - "/td" -> "/time set day": By using the simple "/td" command you can set the time to day in the world 
+ - "/ts" -> "/time set": Now you can use commands such as "/ts night" to set night in the world 
+ - (console) "kx" -> "kick xoft I hate you because": Now you can kick me really fast using the console command "kx no reason" 
+ 	
 
 # Commands
 
@@ -14,6 +21,6 @@ Allows players to define aliases for in-game commands and even chat messages. Se
 # Permissions
 | Permissions | Description | Commands | Recommended groups |
 | ----------- | ----------- | -------- | ------------------ |
-| aliases.alias.list |  | `/alias list` |  |
-| aliases.alias.remove |  | `/alias remove` |  |
-| aliases.alias.set |  | `/alias set` |  |
+| aliases.alias.list | Allows players to show a list of all their currently defined aliases | `/alias list` | everyone |
+| aliases.alias.remove | Allows players to remove an alias they have defined earlier | `/alias remove` | everyone |
+| aliases.alias.set | Allows players to define their aliases | `/alias set` | everyone |
